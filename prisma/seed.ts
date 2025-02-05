86a385481d976bea4447b1491f79cb55b5f9f7f8
DELHAYE Florian <delhaye2005@gmail.com>
2025-02-05 11:29:45 +0100

    Ajout d'un pokémon dans la bdd, getter pour la liste des pokémons

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
+ 
   await prisma.type.deleteMany();
   await prisma.type.createMany({
     data: [
@@ -27,12 +28,26 @@ async function main() {
     ],
   });
 
+  await prisma.pokemonCard.deleteMany();
+  await prisma.pokemonCard.create({
+    data: {
+      name: 'Bulbizarre',
+      pokedexId: 1,
+      typeId: 1,
+      lifePoints: 45,
+      weight: 6.9,
+      size: 0.7,
+      imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'
+    },
+  });
+  
   console.log('Seed completed!');
 }
 
 main()
   .catch((e) => {
-    throw e;
+    console.error(e);
+    process.exit(1);
   })
   .finally(async () => {
     await prisma.$disconnect();
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 export const app = express();
 const port = process.env.PORT || 3000;
+const prisma = new PrismaClient();
 
 app.use(express.json());
 
@@ -10,3 +12,28 @@ export const server = app.listen(port);
 export function stopServer() {
   server.close();
 }
+
+// Accueil
+app.get('/', (req, res) => {
+  res.json({ message: 'Bienvenue 👋' });
+});
+
+// Listes des pokémons
+app.get('/pokemons-cards', async (req, res) => {
+  try {
+    const pokemonCards = await prisma.pokemonCard.findMany();
+    res.json(pokemonCards);
+  } catch (error) {
+    res.status(500).json({ error: 'Une erreur est survenue' });
+  }
+});
+
+// Pokémon spécifique
+app.get('/pokemons-cards', async (req, res) => {
+  try {
+    const pokemonCards = await prisma.pokemonCard.findMany();
+    res.json(pokemonCards);
+  } catch (error) {
+    res.status(500).json({ error: 'Une erreur est survenue' });
+  }
+});
